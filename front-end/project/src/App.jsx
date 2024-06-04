d562239b335fa22d9f152d276d3de75e5f3d7b2f
Jhonnatas Ribeiro <97709889+JhonnatasRibeiroDev@users.noreply.github.com>
2024-06-04 12:31:42 -0300

    added basic project

---
+++ front-end/project/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+function App(){
+    return (
+        <div>
+            <h1>MotoMaster</h1>
+            <p>MotoMaster é o seu aplicativo definitivo para gestão de motos e serviços de manutenção.</p>
+        </div>
+    )
+}export default App;
